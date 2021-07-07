2f92b4de74947c5401d1aab8b146292d329565d6
Alexey Antonov <alex.antonov150@gmail.com>
2021-07-07 11:34:08 +0300

    Disable search button onQueryChange.

--- app/src/main/java/com/aleksejantonov/tajikair/ui/main/MainViewModel.kt
+++ app/src/main/java/com/aleksejantonov/tajikair/ui/main/MainViewModel.kt
@@ -7,7 +7,6 @@ import com.aleksejantonov.tajikair.model.CitiesRepository
 import com.aleksejantonov.tajikair.navigation.AppRouter
 import com.aleksejantonov.tajikair.ui.Screens.*
 import com.aleksejantonov.tajikair.ui.base.BaseViewModel
-import com.aleksejantonov.tajikair.util.cityStub
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.flow.*
@@ -20,14 +19,14 @@ class MainViewModel @Inject constructor(
   @DispatcherDefault private val dispatcherDefault: CoroutineDispatcher,
 ) : BaseViewModel() {
 
-  private val _departureLocationData = MutableStateFlow(cityStub())
-  val departureLocationData: StateFlow<City> = _departureLocationData
+  private val _departureLocationData = MutableSharedFlow<City>(replay = 1, onBufferOverflow = BufferOverflow.DROP_OLDEST)
+  val departureLocationData: SharedFlow<City> = _departureLocationData
 
-  private val _destinationLocationData = MutableStateFlow(cityStub())
-  val destinationLocationData: StateFlow<City> = _destinationLocationData
+  private val _destinationLocationData = MutableSharedFlow<City>(replay = 1, onBufferOverflow = BufferOverflow.DROP_OLDEST)
+  val destinationLocationData: SharedFlow<City> = _destinationLocationData
 
-  private val _enableSearchData = MutableSharedFlow<Boolean>(replay = 1, onBufferOverflow = BufferOverflow.DROP_OLDEST)
-  val enableSearchData: SharedFlow<Boolean> = _enableSearchData
+  private val _enableSearchData = MutableStateFlow(false)
+  val enableSearchData: StateFlow<Boolean> = _enableSearchData
 
   private val _departureQueryData = MutableSharedFlow<String>()
   private val _destinationQueryData = MutableSharedFlow<String>()
@@ -67,12 +66,14 @@ class MainViewModel @Inject constructor(
 
   fun departureQueryChanged(query: String) {
     viewModelScope.launch(dispatcherDefault + exceptionHandler) {
+      _enableSearchData.emit(false)
       _departureQueryData.emit(query)
     }
   }
 
   fun destinationQueryChanged(query: String) {
     viewModelScope.launch(dispatcherDefault + exceptionHandler) {
+      _enableSearchData.emit(false)
       _destinationQueryData.emit(query)
     }
   }
@@ -95,7 +96,7 @@ class MainViewModel @Inject constructor(
   fun onSearchClick() {
     router.forward(
       MAP_FRAGMENT,
-      _departureLocationData.value to _destinationLocationData.value
+      _departureLocationData.replayCache[0] to _destinationLocationData.replayCache[0]
     )
   }
 }
